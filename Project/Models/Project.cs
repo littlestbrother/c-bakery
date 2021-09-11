9216c6a22522b0cb5a2cca0ea82621870c7a6fe3
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-09-10 19:22:18 -0700

    init git

---
+++ Project/Models/Project.cs
@@ -0,0 +1,24 @@
+
+namespace Calendar
+{
+    public class LeapYear
+    {
+        static void Main(){
+        public bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return year % 4 == 0;
+            }
+        }
+    }
+    }
+}
