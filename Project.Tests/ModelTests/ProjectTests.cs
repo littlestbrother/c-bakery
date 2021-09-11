3294c9a5778721f4adf3b2af8029727fc26bff53
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-09-10 19:39:01 -0700

    setup basic c# testing enviroment

--- Project.Tests/ModelTests/ProjectTests.cs
+++ Project.Tests/ModelTests/ProjectTests.cs
@@ -1,37 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Calendar;
+using System;
+using Bakery;
 
-namespace Calendar.Tests
-{
-    [TestClass]
-    public class LeapYearTests
-    {
-        [TestMethod]
-        public void IsLeapYear_NumberDivisibleByFour_True()
-        {
-            LeapYear testLeapYear = new LeapYear();
-            Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
-        }
-
-        [TestMethod]
-        public void IsLeapYear_NumberDivisibleByFour_False()
-        {
-            LeapYear testLeapYear = new LeapYear();
-            Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
-        }
-
-        [TestMethod]
-        public void IsLeapYear_MultipleOfOneHundred_False()
-        {
-            LeapYear testLeapYear = new LeapYear();
-            Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
-        }
-
-        [TestMethod]
-        public void IsLeapYear_MultipleOfFourHundred_True()
-        {
-            LeapYear testLeapYear = new LeapYear();
-            Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
-        }
-    }
-}
+// namespace Bakery.Tests
+// {
+//     [TestClass]
+//     public class BakeryTests
+//     {
+//         [TestMethod]
+//         public void IsBakery_Method_True()
+//         {
+//             Bakery newOrder = new Bakery();
+//             Assert.AreEqual(true, newOrder.Method('value'));
+//         }
+//     }
+// }
